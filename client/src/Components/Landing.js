acecc10d9d4323486d378eff266f2d214d08b80a
Tarun Kantiwal <tarunkantiwal@Taruns-MacBook-Pro.local>
2021-01-25 01:44:46 +0530

    Finalize landing page

--- client/src/Components/Landing.js
+++ client/src/Components/Landing.js
@@ -1,33 +1,35 @@
-import { Container, Row, Col } from "react-bootstrap";
-import iam from '../assets/mediator0.png'
-import iam1 from '../assets/mediator.png'
-import iam2 from '../assets/mediator1.png'
+import { Container, Row, Col, Button } from "react-bootstrap";
 import iam3 from '../assets/back2.jpg'
-import iam4 from '../assets/back3.png'
+import iam from '../assets/kiss.png'
+import iam2 from '../assets/kiss2.png'
 
 
 function Landing() {
     return (
         <div style={{backgroundImage: `url(${iam3})`, backgroundRepeat:'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center'}}>
-            <div style={{ background: 'rgba(1, 1, 1, 0.6)'}}>
+            <div style={{ background: 'rgba(1, 1, 1, 0.7)'}}>
             <Container>
                 <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around'}}>
-                    <div style={{ height: 165 }}></div>
+                    <div style={{ height: 162 }}></div>
                     <Row style={{height:400, justifyContent: 'center' }}>
                         <Col style={{ color: 'white', textAlign: 'center' }}>
                             <div style={{ display: 'flex', flexDirection: 'column' }}>
                                 <div></div>
                                 <div>
+                                    <img src={iam2} height={150}/>
                                     <h1>Welcome to Mediator</h1>
                                     <p>We are here to save you from online fraud and make your transaction very smooth. <br/>
-                                    
+                                    If you don't trust the deal please don't loose your assets on that deal. <br/>
+                                    We will help you to finalize your deal with full protection.
                                     </p>
+                                    <Button style={{marginRight: 10}} variant="primary">Log In</Button>
+                                    <Button style={{marginLeft: 10}} variant="info">Sign Up</Button>
                                 </div>
                                 <div></div>
                             </div>
                         </Col>
                     </Row>
-                    <div style={{ height: 165 }}></div>
+                    <div style={{ height: 163 }}></div>
                 </div>
             </Container>
             </div>
